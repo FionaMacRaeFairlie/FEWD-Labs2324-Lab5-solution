f8994cbf9a4d697aaa4d5286aa0514d369a30376
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2023-07-12 19:56:05 +0100

    Solution for Lab4

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import Home from "./components/Home"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "leaflet/dist/leaflet.css";
-import MapComponent from "./components/Map3"
 
 
 function App() {
   return (
     <div>
       <Home />
-      <MapComponent />
     </div>
   );
 }
