5cd9c285503423e26f07ec77f74be1d2b87a4e2a
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2023-07-13 14:46:56 +0100

    Solution for FEWD Lab5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
-import Home from "./components/Home"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "leaflet/dist/leaflet.css";
-
+import People from "./components/People";
+import Person from "./components/Person";
+import Navigation from "./components/NavigationAlt";
+import NoPage from "./components/NoPage";
+import{staff} from './data/staff';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route path="/" element={<Home />} />
+          <Route path="aboutus" element={<People staff={staff}/>}>
+            <Route path=":personId" element={<Person staff={staff} />} />
+          </Route>
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
